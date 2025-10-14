06819224533e388869e98ccc616c8f4153f0dc05
Vihaan Kulkarni <vihaankulkarni29@gmail.com>
2025-10-14 08:05:53 +0530

    federated genome setup.cfg

---
+++ federated_genome_extractor/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="federated_genome_extractor",
+    version="0.1.0",
+    description="Federated genome extraction tool for MutationScan",
+    author="Vihaan Kulkarni",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[],
+)
